0362af237d8cb8b1d58866dfcda8ea376e57cf26
vnxme <46669194+vnxme@users.noreply.github.com>
2026-01-08 14:33:20 +0300

    Exclude firewall and forwarding from the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,6 @@
 
 # Supported environment variables:
 #
-# Firewall & Forwarding
-# FORWARDING   - [true/ipv4/ipv6/false]      - enables/disables IPv4/IPv6 forwarding
-# MASQUERADE   - [true/...]                  - enables masquerade on the default interface
-# PRIVATE_IPV4 - [semicolon separated CIDRs] - IPv4 ranges to masquerade outgoing traffic from
-# PRIVATE_IPV6 - [semicolon separated CIDRs] - IPv6 ranges to masquerade outgoing traffic from
-#
 # VARIABLE   # DESCRIPTION                                 # DEFAULT VALUE          #
 # ---------- # ------------------------------------------- # ---------------------- #
 # CONFS_DEF  # the default interface name                  # wg0                    #
@@ -27,115 +21,6 @@ HOOKS_DIR="${HOOKS_DIR:-./hooks}"
 PIDS=()
 FILES=()
 
-firewall_up() {
-	local M="${MASQUERADE,,:-}"
-
-	if [ -n "$(which iptables)" ] && [ $(iptables -t filter -L 2>/dev/null 1>&2; echo $?) -eq 0 ]; then
-		IPT4='iptables'
-	elif [ -n "$(which iptables-legacy)" ] && [ $(iptables-legacy -t filter -L 2>/dev/null 1>&2; echo $?) -eq 0 ]; then
-		IPT4='iptables-legacy'
-	fi
-
-	if [ -n "${IPT4}" ]; then
-		FIREWALL_IPV4_FILTER="$(${IPT4}-save -t filter || true)"
-		FIREWALL_IPV4_MANGLE="$(${IPT4}-save -t mangle || true)"
-		FIREWALL_IPV4_NAT="$(${IPT4}-save -t nat || true)"
-
-		${IPT4} -t filter -F || true
-		${IPT4} -t mangle -F || true
-		${IPT4} -t nat -F || true
-
-		if [ "${M}" == "true" ]; then
-			local IFACE="$(ip route | grep default | awk '{print $5}')"
-			if [ -n "${IFACE}" ]; then
-				if [ -n "${PRIVATE_IPV4:-}" ]; then
-					local RANGES; IFS=';' read -r -a RANGES <<< "${PRIVATE_IPV4}"
-					local RANGE; for RANGE in "${RANGES[@]}"; do
-						${IPT4} -t nat -A POSTROUTING -s ${RANGE} -o ${IFACE} -j MASQUERADE || true
-					done
-				else
-					${IPT4} -t nat -A POSTROUTING -o ${IFACE} -j MASQUERADE || true
-				fi
-			fi
-		fi
-	fi
-
-	if [ -n "$(which ip6tables)" ] && [ $(ip6tables -t filter -L 2>/dev/null 1>&2; echo $?) -eq 0 ]; then
-		IPT6='ip6tables'
-	elif [ -n "$(which ip6tables-legacy)" ] && [ $(ip6tables-legacy -t filter -L 2>/dev/null 1>&2; echo $?) -eq 0 ]; then
-		IPT6='ip6tables-legacy'
-	fi
-
-	if [ -n "${IPT6}" ]; then
-		FIREWALL_IPV6_FILTER="$(${IPT6}-save -t filter || true)"
-		FIREWALL_IPV6_MANGLE="$(${IPT6}-save -t mangle || true)"
-		FIREWALL_IPV6_NAT="$(${IPT6}-save -t nat || true)"
-
-		${IPT6} -t filter -F || true
-		${IPT6} -t mangle -F || true
-		${IPT6} -t nat -F || true
-
-		if [ "${M}" == "true" ]; then
-			local IFACE="$(ip -6 route | grep default | awk '{print $5}')"
-			if [ -n "${IFACE}" ]; then
-				if [ -n "${PRIVATE_IPV6:-}" ]; then
-					local RANGES; IFS=';' read -r -a RANGES <<< "${PRIVATE_IPV6}"
-					local RANGE; for RANGE in "${RANGES[@]}"; do
-						${IPT6} -t nat -A POSTROUTING -s ${RANGE} -o ${IFACE} -j MASQUERADE || true
-					done
-				else
-					${IPT6} -t nat -A POSTROUTING -o ${IFACE} -j MASQUERADE || true
-				fi
-			fi
-		fi
-	fi
-}
-
-firewall_down() {
-	if [ -n "${IPT4}" ]; then
-		echo "${FIREWALL_IPV4_FILTER}" | ${IPT4}-restore || true
-		echo "${FIREWALL_IPV4_MANGLE}" | ${IPT4}-restore || true
-		echo "${FIREWALL_IPV4_NAT}" | ${IPT4}-restore || true
-	fi
-
-	if [ -n "${IPT6}" ]; then
-		echo "${FIREWALL_IPV6_FILTER}" | ${IPT6}-restore || true
-		echo "${FIREWALL_IPV6_MANGLE}" | ${IPT6}-restore || true
-		echo "${FIREWALL_IPV6_NAT}" | ${IPT6}-restore || true
-	fi
-}
-
-forwarding_up() {
-	FORWARDING_IPV4="$(cat /proc/sys/net/ipv4/ip_forward)"
-	FORWARDING_IPV6_ALL="$(cat /proc/sys/net/ipv6/conf/all/forwarding)"
-	FORWARDING_IPV6_DEF="$(cat /proc/sys/net/ipv6/conf/default/forwarding)"	
-
-	local F="${FORWARDING,,:-}"
-	if [ "${F}" == "true" ]; then
-		echo 1 > /proc/sys/net/ipv4/ip_forward
-		echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
-		echo 1 > /proc/sys/net/ipv6/conf/default/forwarding
-	elif [ "${F}" == "ipv4" ]; then
-		echo 1 > /proc/sys/net/ipv4/ip_forward
-		echo 0 > /proc/sys/net/ipv6/conf/all/forwarding
-		echo 0 > /proc/sys/net/ipv6/conf/default/forwarding
-	elif [ "${F}" == "ipv6" ]; then
-		echo 0 > /proc/sys/net/ipv4/ip_forward
-		echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
-		echo 1 > /proc/sys/net/ipv6/conf/default/forwarding
-	elif [ "${F}" == "false" ]; then
-		echo 0 > /proc/sys/net/ipv4/ip_forward
-		echo 0 > /proc/sys/net/ipv6/conf/all/forwarding
-		echo 0 > /proc/sys/net/ipv6/conf/default/forwarding
-	fi
-}
-
-forwarding_down() {
-	echo "${FORWARDING_IPV4}" > /proc/sys/net/ipv4/ip_forward
-	echo "${FORWARDING_IPV6_ALL}" > /proc/sys/net/ipv6/conf/all/forwarding
-	echo "${FORWARDING_IPV6_DEF}" > /proc/sys/net/ipv6/conf/default/forwarding
-}
-
 hooks() {
 	[ ! -d "${HOOKS_DIR}/$1" ] && mkdir -p "${HOOKS_DIR}/$1"
 	local FILE; for FILE in "${HOOKS_DIR}"/"$1"/*.sh; do
@@ -146,10 +31,6 @@ hooks() {
 }
 
 launch() {
-	# Configure firewall and forwarding
-	firewall_up
-	forwarding_up
-
 	# Call pre-up hooks
 	hooks "pre-up"
 
